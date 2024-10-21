f5d364cba9a084755a7023185578a3e1abfb8287
MCHDevelopment <development@matthias-hochmuth.de>
2024-10-21 20:06:58 +0200

    Einfache Menuefuehrung

--- app/src/main/java/com/mch/philetiptip/NeueMeldung.java
+++ app/src/main/java/com/mch/philetiptip/NeueMeldung.java
@@ -1,6 +1,9 @@
 package com.mch.philetiptip;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,27 @@ public class NeueMeldung extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_neue_meldung);
+
+        configureButtons();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void configureButtons(){
+        configureHauptmenueButton();
+    }
+
+    private void configureHauptmenueButton(){
+        Button buttonHauptmenue = findViewById(R.id.button_hauptmenue);
+        buttonHauptmenue.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(NeueMeldung.this, PhileMenue.class));
+            }
         });
     }
 }
